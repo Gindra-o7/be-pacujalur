90e65c7a7d247045f0b9633a59334449e31e5bff
Gindra-o7 <gilangindra734@gmail.com>
2025-09-19 15:40:52 +0700

    🔥feat: update Jalur interface to include optional deskripsi field and improve CreateJalurRequest structure

--- src/types/jalur.type.ts
+++ src/types/jalur.type.ts
@@ -1,11 +1,17 @@
+import { Galeri } from "./galeri.type";
+import { Medsos } from "./medsos.type";
+
 export interface Jalur {
-    id: string;
-    nama: string;
-    desa: string;
-    kecamatan: string;
-    kabupaten: string;
-    provinsi: string;
-    image_url?: string | null;
-    deskripsi?: string | null;
-    url_medsos?: string | null;
+  id: string;
+  nama: string;
+  desa: string;
+  kecamatan: string;
+  kabupaten: string;
+  provinsi: string;
+  deskripsi?: string | null;
+}
+
+export interface CreateJalurRequest extends Omit<Jalur, "id"> {
+  medsos: Omit<Medsos, "id" | "jalur_id">[];
+  galeri: Omit<Galeri, "id" | "jalur_id">[];
 }
