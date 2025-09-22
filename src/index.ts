f0b18ebfe52083831217c152a9fb50dcf9efa884
Gindra-o7 <gilangindra734@gmail.com>
2025-09-22 12:30:12 +0700

    🔥feat: refactor index.ts to improve router organization and update CORS settings

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import jalurRouter from './routes/jalur.route';
 import path from 'path';
+import globalRouter from './routes/global.route';
+import jalurRouter from './routes/jalur.route';
+import penginapanRouter from './routes/penginapan.route';
+import fasilitasRouter from './routes/fasilitas.route';
 
 dotenv.config();
 
 const app: Express = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}));
+app.use(cors({ origin: "*" }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-app.use(jalurRouter);
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Welcome to Paju Jalur API!');
-});
+app.use(globalRouter);
+app.use(jalurRouter);
+app.use(penginapanRouter);
+app.use(fasilitasRouter);
+
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
