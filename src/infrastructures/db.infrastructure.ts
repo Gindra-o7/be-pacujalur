4f266b4174b76c02502f4029c698486e775cf0be
Gindra-o7 <gilangindra734@gmail.com>
2025-09-23 09:33:28 +0700

    ⚙refactor: migrate from postgresql to mysql

--- src/infrastructures/db.infrastructure.ts
+++ src/infrastructures/db.infrastructure.ts
@@ -1,9 +1,9 @@
-import { Pool } from "pg";
+import mysql from "mysql2/promise"
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Pool({
+const db = mysql.createPool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
