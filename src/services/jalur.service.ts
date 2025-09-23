126fe04d16eb8931b312725f20046c667c74baae
Gindra-o7 <gilangindra734@gmail.com>
2025-09-23 09:34:51 +0700

    🔥feat: add service for jalur

--- src/services/jalur.service.ts
+++ src/services/jalur.service.ts
@@ -1,9 +1,13 @@
+import PaginationHelper from "../helpers/pagination.helper";
 import JalurRepository from "../repositories/jalur.repository";
-import { CreateJalurRequest, Jalur } from "../types/jalur.type";
+import { CreateJalurRequest } from "../types/jalur.type";
 
 export default class JalurService {
-  public static async get(): Promise<Jalur[]> {
-    return JalurRepository.find();
+  public static async getAll(page: number, limit: number){
+    const offset = PaginationHelper.getOffset(page, limit);
+    const { data, total } = await JalurRepository.findAll(offset, limit);
+
+    return PaginationHelper.buildPaginatedResponse(data, page, limit, total);
   }
 
   public static async post(data: CreateJalurRequest) {
