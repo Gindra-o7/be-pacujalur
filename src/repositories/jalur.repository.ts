2e24cd273d923b1342df84e6a229b287f7a0dee0
Gindra-o7 <gilangindra734@gmail.com>
2025-09-23 09:33:58 +0700

    🔥feat: repository for jalur

--- src/repositories/jalur.repository.ts
+++ src/repositories/jalur.repository.ts
@@ -1,55 +1,56 @@
-import { PoolClient } from "pg";
 import db from "../infrastructures/db.infrastructure";
 import { CreateJalurRequest, Jalur } from "../types/jalur.type";
 import { Medsos } from "../types/medsos.type";
 import { Galeri } from "../types/galeri.type";
+import { PoolConnection } from "mysql2/promise";
 
 export default class JalurRepository {
   public static async findAll(offset: number, limit: number): Promise<{ data: Jalur[]; total: number }> {
     const countQuery = "SELECT COUNT(*) as total FROM jalur";
-    const countResult = await db.query(countQuery);
-    const total = parseInt(countResult.rows[0].total);
+    const [countResult]: any = await db.query(countQuery);
+    const total = parseInt(countResult[0].total);
 
     const query = `
       SELECT id, nama, desa, kecamatan, kabupaten, provinsi, deskripsi
       FROM jalur
       ORDER BY nama
-      LIMIT $1 OFFSET $2
+      LIMIT ? OFFSET ?
     `;
-    const result = await db.query(query, [limit, offset]);
+    const [result]: any = await db.query(query, [limit, offset]);
 
     return {
-      data: result.rows,
+      data: result,
       total,
     };
   }
 
   public static async create(data: CreateJalurRequest): Promise<Jalur & { medsos: Medsos[]; galeri: Galeri[] }> {
-    const client: PoolClient = await db.connect();
+    const client: PoolConnection = await db.getConnection();
     try {
-      await client.query("BEGIN");
+      await client.beginTransaction();
       const jalurSql = `
             INSERT INTO jalur (nama, desa, kecamatan, kabupaten, provinsi, deskripsi)
-            VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING *;
+            VALUES (?, ?, ?, ?, ?, ?);
         `;
       const jalurParams = [data.nama, data.desa, data.kecamatan, data.kabupaten, data.provinsi, data.deskripsi];
-      const jalurResult = await client.query(jalurSql, jalurParams);
-      const jalurBaru: Jalur = jalurResult.rows[0];
-
-      const jalurId = jalurBaru.id;
+      const [jalurResult]: any = await client.query(jalurSql, jalurParams);
+      const jalurId = jalurResult.insertId;
+      
+      const [getJalur]: any = await client.query("SELECT * FROM jalur WHERE id = ?", [jalurId]);
+      const jalurBaru: Jalur = getJalur[0];
 
       const medsosBaru: Medsos[] = [];
       if (data.medsos && data.medsos.length > 0) {
         for (const item of data.medsos) {
           const medsosSql = `
                     INSERT INTO medsos (media, link, jalur_id)
-                    VALUES ($1, $2, $3)
-                    RETURNING *;
+                    VALUES (?, ?, ?);
                 `;
           const medsosParams = [item.media, item.link, jalurId];
-          const medsosResult = await client.query(medsosSql, medsosParams);
-          medsosBaru.push(medsosResult.rows[0]);
+          const [medsosResult]: any = await client.query(medsosSql, medsosParams);
+          
+          const [getMedsos]: any = await client.query("SELECT * FROM medsos WHERE id = ?", [medsosResult.insertId]);
+          medsosBaru.push(getMedsos[0]);
         }
       }
 
@@ -58,20 +59,21 @@ export default class JalurRepository {
         for (const item of data.galeri) {
           const galeriSql = `
                     INSERT INTO galeri (image_url, judul, caption, jalur_id)
-                    VALUES ($1, $2, $3, $4)
-                    RETURNING *;
+                    VALUES (?, ?, ?, ?);
                 `;
           const galeriParams = [item.image_url, item.judul, item.caption, jalurId];
-          const galeriResult = await client.query(galeriSql, galeriParams);
-          galeriBaru.push(galeriResult.rows[0]);
+          const [galeriResult]: any = await client.query(galeriSql, galeriParams);
+          
+          const [getGaleri]: any = await client.query("SELECT * FROM galeri WHERE id = ?", [galeriResult.insertId]);
+          galeriBaru.push(getGaleri[0]);
         }
       }
 
-      await client.query("COMMIT");
+      await client.commit();
 
       return { ...jalurBaru, medsos: medsosBaru, galeri: galeriBaru };
     } catch (error) {
-      await client.query("ROLLBACK");
+      await client.rollback();
       console.error("Transaction ROLLBACK:", error);
       throw error;
     } finally {
