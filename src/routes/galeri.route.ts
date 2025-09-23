df20e766c8dd24024c70bf349534f502e74ff16a
Gindra-o7 <gilangindra734@gmail.com>
2025-09-23 09:34:20 +0700

    🔥feat: add route for galeri

--- src/routes/galeri.route.ts
+++ src/routes/galeri.route.ts
@@ -1,1 +1,8 @@
 import { Router } from "express";
+import GaleriHandler from "../handlers/galeri.handler";
+
+const galeriRouter = Router();
+
+galeriRouter.get("/galeri", GaleriHandler.getAll);
+
+export default galeriRouter;
