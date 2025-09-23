0d6ea05612a26cb1870ca2a9385519ff18d794a3
Gindra-o7 <gilangindra734@gmail.com>
2025-09-23 09:34:27 +0700

    🔥feat: add route for jalur

--- src/routes/jalur.route.ts
+++ src/routes/jalur.route.ts
@@ -3,7 +3,7 @@ import JalurHandler from "../handlers/jalur.handler";
 
 const jalurRouter = Router();
 
-jalurRouter.get("/jalur", JalurHandler.get);
+jalurRouter.get("/jalur", JalurHandler.getAll);
 jalurRouter.post("/jalur", JalurHandler.post);
 
 export default jalurRouter;
