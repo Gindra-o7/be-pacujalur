ff935a973f4467432d735d5cc0ff39bb7b407523
Gindra-o7 <gilangindra734@gmail.com>
2025-09-23 09:33:50 +0700

    🔥feat: repository for galeri

--- src/repositories/galeri.repository.ts
+++ src/repositories/galeri.repository.ts
@@ -4,19 +4,19 @@ import { Galeri } from "../types/galeri.type";
 export default class GaleriRepository {
   public static async findAll(offset: number, limit: number): Promise<{ data: Galeri[]; total: number }> {
     const countQuery = "SELECT COUNT(*) as total FROM galeri";
-    const countResult = await db.query(countQuery);
-    const total = parseInt(countResult.rows[0].total);
+    const [countResult]: any = await db.query(countQuery);
+    const total = parseInt(countResult[0].total);
 
     const query = `
       SELECT id, image_url, judul, caption, jalur_id
       FROM galeri
       ORDER BY judul
-      LIMIT $1 OFFSET $2
+      LIMIT ? OFFSET ?
     `;
-    const result = await db.query(query, [limit, offset]);
+    const [result]: any = await db.query(query, [limit, offset]);
 
     return {
-      data: result.rows,
+      data: result,
       total,
     };
   }
